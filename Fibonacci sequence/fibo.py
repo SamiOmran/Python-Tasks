84e6a3acd66e59a7b5567599690a1dfb9334e053
Sami Imran <samiimran2016@gmail.com>
2022-11-28 17:22:42 +0200

    little edit

--- Fibonacci sequence/fibo.py
+++ Fibonacci sequence/fibo.py
@@ -1,9 +1,9 @@
 def fibo(numb):
     if not numb:
         return 0
-    elif numb <=2:
+    elif numb <= 2:
         return 1
     return fibo(numb-1) + fibo(numb-2)
 
 
-print(fibo(-2))
+print(fibo(9))
